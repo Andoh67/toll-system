066e208909293b74246c9ac4c0c2a93cce0ca4a3
Stephen Baffoe <baffoestephen980@gmail.com>
2025-09-30 20:53:18 +0000

    fix: use environment variables for Firebase instead of file

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 import axios from "axios";
 
 const app = express();
 app.use(bodyParser.json());
 
-// -------------------- Fix for __dirname in ESM --------------------
-const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(__filename);
-
-// -------------------- Load Firebase Service Account --------------------
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(_dirname, "serviceAccountKey.json"), "utf8")
-);
+// -------------------- Firebase Service Account from Environment Variables --------------------
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
 
 // -------------------- Firebase Init --------------------
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://automated-toll-system-c2f6c-default-rtdb.firebaseio.com/"
+  databaseURL: process.env.FIREBASE_DATABASE_URL || "https://automated-toll-system-c2f6c-default-rtdb.firebaseio.com/"
 });
 const db = admin.database();
 
